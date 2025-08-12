c59e0019f0a07f2bac6962de7e4be7bbcac99f45
andirajugayathri <andirajugayathri92@gmail.com>
2025-08-12 15:35:12 +0530

    updated

---
+++ send_estimate.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $mobile = htmlspecialchars($_POST['mobile']);
+    $location = htmlspecialchars($_POST['location']);
+
+    $to = "rajaadonai@gmail.com"; // Replace with your email
+    $subject = "New Full Home Interior Estimate Request";
+    $message = "
+    New estimate request details:
+
+    Name: $name
+    Mobile: $mobile
+    Location: $location
+    ";
+    $headers = "From: noreply@yourdomain.com\r\n";
+    $headers .= "Reply-To: $mobile\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        header("Location: thankyou.html");
+        exit;
+    } else {
+        echo "Error sending email. Please try again.";
+    }
+}
+?>
